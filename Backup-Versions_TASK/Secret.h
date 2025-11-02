c7bbae2825b5232c424cc36c678204e2ac04ff7b
Mats Dumonceau <mats.dumonceau.md@gmail.com>
2025-11-02 22:50:49 +0100

    added Secret.h file

---
+++ Backup-Versions_TASK/Secret.h
@@ -0,0 +1,8 @@
+// WIFI credentials
+#define SECRET_SSID "E109-E110"
+#define SECRET_PASS "DBHaacht24"
+
+// Mosquitto Broker credentials
+#define BROKER "192.168.0.xxx" // IP adres van de MQTT host
+#define PORT 1883
+#define TOPIC "x"   // Topic waarop je zal publiceren in MQTT
